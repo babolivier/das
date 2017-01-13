350a50155c3ed529a91aa0c09fa20e286014a031
Brendan Abolivier <contact@brendanabolivier.com>
2017-01-13 16:47:59 +0100

    Basic PHP service with auth based on user category

---
+++ service1/index.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use Macaroons\Macaroon;
+use Macaroons\Verifier;
+
+if(!isset($_COOKIE['das-macaroon'])) {
+	echo "Not logged in";
+	exit();
+}
+
+$serialised = $_COOKIE['das-macaroon'];
+
+$m = Macaroon::deserialize($serialised);
+$v = new Verifier();
+
+
+$v->setCallbacks([
+	function($a) {
+		return !strcmp($a, "status = teacher");
+	}
+]);
+
+try {
+	$bool = $v->verify($m, 'pocsecret');
+} catch(Exception $e) {
+	$bool = false;
+}
+
+if($bool) {
+	echo "Access granted.\n<br />Welcome ".$m->getIdentifier()."!";
+} else {
+	echo "Access denied. Service is restricted to teachers.";
+}
