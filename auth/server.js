e7d6e2d104352a451087fa3143f3be16251841c7
Brendan Abolivier <contact@brendanabolivier.com>
2017-01-13 16:46:47 +0100

    Ugly auth server

---
+++ auth/server.js
@@ -0,0 +1,51 @@
+var MacaroonsBuilder	= require('macaroons.js').MacaroonsBuilder;
+var MacaroonsVerifier	= require('macaroons.js').MacaroonsVerifier;
+var express		= require('express');
+var app			= express();
+var bodyParser		= require('body-parser');
+
+var location = "https://ent.brendanabolivier.com";
+var secretKey = "pocsecret";
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res, next) {
+	res.sendFile(__dirname + '/form.html');
+});
+
+app.post('/', function(req, res, next) {
+	var identifier = req.body.username;
+
+	var m = new MacaroonsBuilder(location, secretKey, identifier)
+		.add_first_party_caveat("status = student")
+		.getMacaroon();
+
+	res.cookie('das-macaroon', m.serialize());
+
+	res.send('Logged in as ' + req.body.username + ' (student)');
+});
+
+app.get('/teacher', function(req, res, next) {
+	res.sendFile(__dirname + '/form.html');
+});
+
+app.post('/teacher', function(req, res, next) {
+	var identifier = req.body.username;
+
+	var m = new MacaroonsBuilder(location, secretKey, identifier)
+		.add_first_party_caveat("status = teacher")
+		.getMacaroon();
+
+	res.cookie('das-macaroon', m.serialize());
+
+	res.send('Logged in as ' + req.body.username + ' (teacher)');
+});
+
+app.listen(1337, function() {
+	console.log('Server started');
+});
+
